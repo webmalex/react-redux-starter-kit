5d9ddd28662bbbe426758d3de4aa7f552d4f35ec
Alexander Efimov <alexandr.efimov@gmail.com>
2016-12-12 22:10:26 +0500

    Responsive

--- src/routes/Grid/components/GridView.js
+++ src/routes/Grid/components/GridView.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import ReactGridLayout from 'react-grid-layout'
+
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
-
 import './GridView.scss'
+
+// import ReactGridLayout from 'react-grid-layout'
+import {Responsive, WidthProvider} from 'react-grid-layout';
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 const MyFirstGrid = React.createClass({
   render: function() {
@@ -12,15 +16,19 @@ const MyFirstGrid = React.createClass({
       {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
       {i: 'c', x: 4, y: 0, w: 1, h: 2}
     ];
+    var layouts = {lg: layout}
     return (
-      <ReactGridLayout layout={layout} cols={12} rowHeight={30} width={1200}>
+    <ResponsiveReactGridLayout className="layout" layouts={layouts}
+      breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
+      cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}>
         <div key={'a'}>a</div>
         <div key={'b'}>b</div>
         <div key={'c'}>c</div>
-      </ReactGridLayout>
+      </ResponsiveReactGridLayout>
     )
   }
 });
+
 
 export const GridView = () => (
   <MyFirstGrid />
